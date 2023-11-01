3eda54159c3f4147670988e3e607862e25349698
LautaP3 <lautape04@gmail.com>
2023-11-01 16:41:36 -0300

    e7

--- js/my-profile.js
+++ js/my-profile.js
@@ -29,3 +29,77 @@ profileImageInput.addEventListener("change", (event) => {
       }, false)
     })
   })()
+  function saveUserDataToLocalStorage() {
+    const userData = {
+      firstName: document.getElementById("nameInput1").value,
+      secondName: document.getElementById("nameInput2").value,
+      lastName1: document.getElementById("lastNameInput1").value,
+      lastName2: document.getElementById("lastNameInput2").value,
+      email: document.getElementById("emailInput").value,
+      phoneNumber: document.getElementById("phoneInput").value,
+    };
+  
+    localStorage.setItem("userData", JSON.stringify(userData));
+  
+    // Guarda la imagen de perfil si se seleccionó una
+    if (profileImageInput.files.length > 0) {
+      const selectedImage = profileImageInput.files[0];
+      const imageURL = URL.createObjectURL(selectedImage);
+      localStorage.setItem("profileImage", imageURL);
+    }
+  }
+  
+  // Función para cargar los datos del usuario y la imagen de perfil desde el localStorage
+  function saveUserDataToLocalStorage() {
+    const userData = {
+      firstName: document.getElementById("nameInput1").value,
+      secondName: document.getElementById("nameInput2").value,
+      lastName1: document.getElementById("lastNameInput1").value,
+      lastName2: document.getElementById("lastNameInput2").value,
+      email: document.getElementById("emailInput").value,
+      phoneNumber: document.getElementById("phoneInput").value,
+    };
+  
+    localStorage.setItem("userData", JSON.stringify(userData));
+  
+    // Guarda la imagen de perfil si se seleccionó una
+    if (profileImageInput.files.length > 0) {
+      const selectedImage = profileImageInput.files[0];
+      const imageURL = URL.createObjectURL(selectedImage);
+      localStorage.setItem("profileImage", imageURL);
+    }
+  }
+  
+  // Función para cargar los datos del usuario y la imagen de perfil desde el localStorage
+  function saveUserDataToLocalStorage() {
+    const userData = {
+      firstName: document.getElementById("nameInput1").value,
+      secondName: document.getElementById("nameInput2").value,
+      lastName1: document.getElementById("lastNameInput1").value,
+      lastName2: document.getElementById("lastNameInput2").value,
+      email: document.getElementById("emailInput").value,
+      phoneNumber: document.getElementById("phoneInput").value,
+    };
+  
+    localStorage.setItem("userData", JSON.stringify(userData));
+  }
+  
+  // Función para cargar los datos del usuario y la imagen de perfil desde el localStorage
+  function loadUserDataFromLocalStorage() {
+    const userData = JSON.parse(localStorage.getItem("userData"));
+  
+    if (userData) {
+      document.getElementById("nameInput1").value = userData.firstName;
+      document.getElementById("nameInput2").value = userData.secondName;
+      document.getElementById("lastNameInput1").value = userData.lastName1;
+      document.getElementById("lastNameInput2").value = userData.lastName2;
+      document.getElementById("emailInput").value = userData.email;
+      document.getElementById("phoneInput").value = userData.phoneNumber;
+    }
+  }
+  
+  loadUserDataFromLocalStorage();
+  
+  const saveButton = document.getElementById("saveChanges");
+  saveButton.addEventListener("click", saveUserDataToLocalStorage);
+  
